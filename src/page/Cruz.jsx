805bbb3e694a2fdbdcbb07ad594fad95ee5d11fb
cruz-emman <113968577+cruz-emman@users.noreply.github.com>
2023-03-01 18:24:01 +0800

    Zsa
    asdsa

--- src/page/Cruz.jsx
+++ src/page/Cruz.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Cruz = () => {
   return (
+<<<<<<< Updated upstream
     <div>Cru asdsad asdasds ASDSADaz</div>
+=======
+    <div>
+      This is emmanasds
+    </div>
+>>>>>>> Stashed changes
   )
 }
 
