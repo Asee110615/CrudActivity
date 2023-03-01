0f30fdfce7412f6f47d1b1bdb23586b05faf90c1
pulutan123 <zorenbaguilar@tua.edu.ph>
2023-03-01 18:27:23 +0800

    test
    123

--- src/page/Zoren.jsx
+++ src/page/Zoren.jsx
@@ -1,8 +1,42 @@
+import { Typography, Box,   } from '@mui/material'
 import React from 'react'
+
+
+
+
 
 const Zoren = () => {
   return (
-    <div>Zoren</div>
+    
+
+    <Box sx={{ width:"100%", height:"100%", bgcolor: 'warning.main', }}>
+          <Typography style={{color:'blue'}}  variant="h2" component="h2">
+          Collegio De San Lorenzo
+          </Typography>
+    <Box></Box>
+        
+        
+    <Box sx={{  display: 'flex', justifyContent:'center', alignItems: 'center' , }}>
+           <Typography variant="h4" component="h6"> Register </Typography>
+    </Box>
+    
+    </Box>
+    
+    
+    
+    
+    
+  
+    
+
+
+
+
+
+
+
+
+    
   )
 }
 
