7d333693923e9f73041edcaa78f9f8ef646c972a
rensteve33122 <126432182+rensteve33122@users.noreply.github.com>
2023-03-02 14:44:35 +0800

    eto na
    si chito miranda

--- src/page/Rensteve.jsx
+++ src/page/Rensteve.jsx
@@ -1,32 +1,62 @@
-import { Box, Button, TextField } from '@mui/material'
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Box, Button, TextField, Typography } from '@mui/material';
 
-const Rensteve = () => {
+const mockData = [
+  { id: 1, name: 'John' },
+  { id: 2, name: 'Jane' },
+  { id: 3, name: 'Bob' },
+];
+
+const CrudExample = () => {
+  const [data, setData] = useState([]);
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    setData(mockData);
+  }, []);
+
+  const handleCreate = () => {
+    const newData = [...data, { id: data.length + 1, name }];
+    setData(newData);
+    setName('');
+  };
+
+  const handleUpdate = (id, newName) => {
+    const updatedData = data.map(item =>
+      item.id === id ? { ...item, name: newName } : item
+    );
+    setData(updatedData);
+  };
+
+  const handleDelete = id => {
+    const filteredData = data.filter(item => item.id !== id);
+    setData(filteredData);
+  };
+
   return (
-
-
-   
-    <Box sx={{ width: '100%', height: '100%' ,boxShadow: 4,  bgcolor: 'grey', flexDirection: 'column',display: 'flex',pt: 4, gap:2}}>
-      
-      <Box sx={{ bgcolor: 'green',pt: 4}}>  <TextField id="filled-basic" label="item name" variant="filled" />
-            <TextField id="outlined-basic" label="item description" variant="outlined" />
-            <Button variant="contained">Add here</Button>
-            <Button variant="contained">Edit</Button>
-            <Button variant="outlined">Delete</Button>
+    <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+      <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', mt: 2 }}>
+        <TextField label="Name" value={name} onChange={e => setName(e.target.value)} />
+        <Button variant="contained" onClick={handleCreate}>
+          Create
+        </Button>
       </Box>
-      
-      <Box sx={{ bgcolor: 'green', pt: 4,}}>  <TextField id="filled-basic" label="item name" variant="filled" />
-            <TextField id="outlined-basic" label="item description" variant="outlined" />
-            <Button variant="contained">Add here</Button>
-            <Button variant="contained">Edit</Button>
-            <Button variant="outlined">Delete</Button>
+      <Box sx={{ display: 'flex', flexDirection: 'row', mt: 2 }}>
+        {data.map(item => (
+          <Box key={item.id} sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', mb: 1 }}>
+            <TextField
+              label="Name"
+              value={item.name}
+              onChange={e => handleUpdate(item.id, e.target.value)}
+            />
+            <Button variant="contained" onClick={() => handleDelete(item.id)}>
+              Delete
+            </Button>
+          </Box>
+        ))}
       </Box>
-      
-    
     </Box>
+  );
+};
 
-  )
-}
-
-export default Rensteve
-
+export default CrudExample;
